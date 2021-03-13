7685f33ba6091a3a78453790cd0ca369b560bfaa
Keshav Agarwal <keshavagarwal1710@gmail.com>
2021-03-13 19:59:35 +0530

    Added Faculty login fe

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const studentRegister = require('./routes/student-register')
 const signRouter = require("./routes/student-login")
 const adminRouter = require("./routes/admin-login")
 const facultyRouter = require('./routes/f-dashboard')
+const facultySigninRoute = require("./routes/faculty-login")
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -26,6 +27,7 @@ app.use('/student-login',signRouter);
 app.use('/admin-login',adminRouter);
 app.use('/student-register',studentRegister);
 app.use('/f-dashboard',facultyRouter);
+app.use('/faculty-login',facultySigninRoute);
 
 app.listen(port,(req,res)=>{
     console.log("up and running")
