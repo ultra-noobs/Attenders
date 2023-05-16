f09f3125f361faf04d26614978b21873073da52b
Mayank Agrawal <agrawalmayank168@gmail.com>
2023-05-16 23:26:10 +0530

    changes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,10 @@
+// he code starts by defining a video element in the HTML
 const video = document.getElementById("video");
 
 const startVideo =()=>{
     // console.log("yes")
+    // navigator.getUserMedia() method to access the webcam
+    //  and display the video in the video element.
     navigator.getUserMedia(
         {video:{}},
         stream =>{
@@ -10,6 +13,14 @@ const startVideo =()=>{
         err => console.error(err)
     )
 }
+
+// Another function startLecture() is defined that is triggered
+// when a button with id start__btn is clicked. 
+// It checks if the input field with id id_input is empty or not. 
+// If it is empty, it displays an error message using the SweetAlert library. 
+// If the input is not empty, it displays the video element and hides the input field and start button.
+//  It then loads the necessary Face-API.js models using the Promise.all() method.
+
 const startLecture = ()=>{
     var input = document.getElementById('id_input');
     var st__btn = document.getElementById('start__btn');
@@ -37,6 +48,19 @@ const startLecture = ()=>{
 }
 
 //starting promises
+// event listner for the video when the webcam isloaded
+
+
+// After that, an event listener is added to the video element
+// to listen to the playing event. When the video starts playing,
+// it creates a canvas element and loads a single image of a person's face using the LoadImage() function.
+//  It then resizes the canvas and runs a detection loop using the setInterval() method 
+//  that detects all faces in the video using the detectAllFaces() method with the TinyFaceDetectorOptions. 
+//  It also uses the withFaceLandmarks() and withFaceDescriptors() methods to extract facial landmarks and descriptors for each detected face.
+
+// It then resizes the detections and uses the FaceMatcher 
+// class to compare each detected face with the loaded image to find the best match. 
+// Finally, it draws the detections on the canvas using the drawDetections() method.
 
 video.addEventListener("playing",async()=>{
     const canvas = faceapi.createCanvasFromMedia(video);
